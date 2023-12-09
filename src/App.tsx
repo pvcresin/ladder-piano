37e842ab83d02facc69710ca9e23a850896e1a04
pvcresin <pvcresin0730@gmail.com>
2023-12-10 01:39:20 +0900

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import { useState } from "react";
-
 import "./App.css";
 
+import React, { useState } from "react";
+import NOTE_KEYS from "./noteKeys";
+
+const MAX_ROW_COUNT = 8;
+const DEFAULT_ROW_COUNT = 5;
+
+const CENTER_ROW = 4;
+
 function App() {
+  const [rowCount, setRowCount] = useState(DEFAULT_ROW_COUNT);
+
   return (
-    <div>
-      <h1>Ladder Piano</h1>
+    <div className="app">
+      <div className="control">
+        <button
+          className="row-button"
+          onClick={() => setRowCount((prev) => prev - 1)}
+        >
+          -
+        </button>
+        <button
+          className="row-button"
+          onClick={() => setRowCount((prev) => prev + 1)}
+        >
+          +
+        </button>
+      </div>
+      <div className="rows">
+        {new Array(rowCount).fill(null).map((_, index) => {
+          const minRowNum = CENTER_ROW - Math.floor(rowCount / 2);
+          const rowIndexFromBottom = rowCount - index - 1;
+          const rowKey = minRowNum + rowIndexFromBottom;
+          return (
+            <ul className="piano-keys" key={index}>
+              <li className="key white" data-key="C">{`C${rowKey}`}</li>
+              <li className="key black" data-key="Db">{`Db${rowKey}`}</li>
+              <li className="key white" data-key="D">{`D${rowKey}`}</li>
+              <li className="key black" data-key="Db">{`Db${rowKey}`}</li>
+              <li className="key white" data-key="E">{`E${rowKey}`}</li>
+              <li className="key white" data-key="F">{`F${rowKey}`}</li>
+              <li className="key black" data-key="Gb">{`Gb${rowKey}`}</li>
+              <li className="key white" data-key="G">{`G${rowKey}`}</li>
+              <li className="key black" data-key="Ab">{`Ab${rowKey}`}</li>
+              <li className="key white" data-key="A">{`A${rowKey}`}</li>
+              <li className="key black" data-key="Bb">{`Bb${rowKey}`}</li>
+              <li className="key white" data-key="B">{`B${rowKey}`}</li>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 
 const rootElement = document.getElementById("root");
 
